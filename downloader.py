c98b40442fde25ffba9dc9d3954348272b1b6ed0
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-12-12 12:59:34 +0530

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,6 +1,7 @@
 import os
 import tempfile
 from pytube import YouTube, Playlist
+from moviepy.editor import VideoFileClip
 
 def download_video(url):
     """Download a single YouTube video."""
@@ -15,3 +16,15 @@ def get_playlist_videos(url):
     """Get all video URLs from a YouTube playlist."""
     playlist = Playlist(url)
     return playlist.video_urls
+
+def generate_thumbnail(video_path):
+    """Generate a thumbnail for the video using MoviePy."""
+    clip = VideoFileClip(video_path)
+    thumbnail_path = os.path.splitext(video_path)[0] + "_thumb.jpg"
+    frame = clip.get_frame(clip.duration / 2)  # Get a frame from the middle of the video
+    clip.close()
+
+    from PIL import Image
+    image = Image.fromarray(frame)
+    image.save(thumbnail_path, "JPEG")
+    return thumbnail_path
